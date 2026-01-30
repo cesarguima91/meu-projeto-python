569377643db4a95403536ce8847797c2816d7076
“cesarguima91 <cevinicius@hotmail.com>
2026-01-30 17:50:45 -0300

    alteracao_teste

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-nome = input("Qual é o seu nome? ")
+nome = input("Olá, qual é o seu nome? ")
 hora_atual = datetime.now().strftime("%H:%M")
 
 print(f"\nOlá, {nome}! Seja bem-vindo(a).")
